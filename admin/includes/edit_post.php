519feb95150f7abe429bc434df6de36385f0b5be
CodeTurret <mushfiq.fuad@gmail.com>
2019-01-20 22:27:31 +0600

    edit post image, which was a bug in previous version

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -21,11 +21,53 @@
         $post_content = $row['post_content'];
         $post_tags = $row['post_tags'];
         $post_comment_counts = $row['post_comment_counts'];
-        //$post_content = $row['post_content'];
-
+        
 
     }
 
+    if(isset($_POST['update_post']))
+    {
+    	$post_author = $_POST['post_author'];
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST['post_category'];
+        $post_status = $_POST['post_status'];
+        //$post_date = $row['post_date'];
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+        $post_content = $_POST['post_content'];
+        $post_tags = $_POST['post_tags'];
+    
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if(empty($post_image))
+    {
+    	$query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+
+    	$select_image = mysqli_query($connection, $query);
+    	while ($row = mysqli_fetch_array($select_image))
+    	{
+    		$post_image = $row['post_image'];
+    	}
+    }
+
+
+    $query = "
+    	UPDATE posts SET
+    	post_title = '{$post_title }', 
+    	post_category_id = '{$post_category_id}', 
+    	post_date = now(), 
+    	post_author = '{$post_author}', 
+    	post_status = '{$post_status}', 
+    	post_tags = '{$post_tags}', 
+    	post_content = '{$post_content}', 
+    	post_image = '{$post_image}'
+    	WHERE post_id = '{$the_post_id}'
+    	";
+
+    	$update_post = mysqli_query($connection, $query);
+
+    	confirmQuery($update_post);
+    }
 
 ?>
 
@@ -37,13 +79,13 @@
 
 	<div class="form-group">
 		<label for="title">Post Title</label>
-		<input type="text" class="form-control" name="title"
+		<input type="text" class="form-control" name="post_title"
 		value="<?php echo $post_title; ?>">
 	</div>
 
 	<div class="form-group">
 		
-		<select>
+		<select name="post_category" id="">
 
 		<?php
 		$query = "SELECT * FROM categories";
@@ -74,7 +116,7 @@
 
 	<div class="form-group">
 		<label for="title">Post Author</label>
-		<input type="text" class="form-control" name="author"
+		<input type="text" class="form-control" name="post_author"
 		value="<?php echo $post_author; ?>">
 	</div>
 
@@ -85,8 +127,9 @@
 	</div>
 
 	<div class="form-group">
+		<label for="post_image">Post Image</label>
 		<img width="100" src="../images/<?php echo $post_image; ?>">
-		 
+		 <input type="file" name="image">
 	</div>
 
 	<div class="form-group">
@@ -103,7 +146,7 @@
 
 	<div class="form-group">
 		
-		<input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+		<input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
 	</div>
 
 
